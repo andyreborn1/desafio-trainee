1570773b23b579cb1ce8445c8224d3bec640466d
Anderson VInicius Ribeiro <andyreborn@hotmail.com.br>
2021-09-02 16:13:46 -0300

    Arquivos necessários para a transferência de dados

---
+++ Data-Modeling-with-Postgres-master/create_tables.py
@@ -0,0 +1,31 @@
+import psycopg2
+from sql_queries import create_table_queries, drop_table_queries
+
+
+def create_database():
+    # connect to default database
+    
+    
+    # create sparkify database with UTF8 encoding
+    
+    # close connection to default database
+    
+    
+    # connect to sparkify database
+    
+    
+    
+def drop_tables(cur, conn):
+    
+
+
+def create_tables(cur, conn):
+    
+
+
+def main():
+    
+
+
+if __name__ == "__main__":
+    main()
